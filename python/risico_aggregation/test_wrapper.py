2126b6b62c4cc8d310474b009ed06d6c11d68654
mirkodandrea <mirko.dandrea@gmail.com>
2025-02-24 15:35:24 +0100

    fix: timestamps in utc

--- python/risico_aggregation/test_wrapper.py
+++ python/risico_aggregation/test_wrapper.py
@@ -1,19 +1,20 @@
 from risico_aggregation import aggregate_stats, compute_intersections
 import geopandas as gpd
+import pandas as pd
 import xarray as xr
-#ds = xr.open_dataset("/opt/risico/RISICO2023/OUTPUT-NC/V.nc")
-ds = xr.open_dataset("~/Downloads/VNDWI.nc")
+ds = xr.open_dataset("/opt/risico/RISICO2023/OUTPUT-NC/V.nc")
+#ds = xr.open_dataset("~/Downloads/VNDWI.nc")
 gdf = gpd.read_file('/opt/risico/AGGREGATION_CACHE/shp/Italia/regioni_ISTAT2001.shp')
 gdf.set_index('COD_REG', inplace=True)
 intersections = compute_intersections(gdf, ds.latitude, ds.longitude)
 
-dsd = ds.V.iloc[0:24]
+dsd = ds.V# .isel(time=slice(72,96))
 
 dfs = aggregate_stats(
     data=dsd,
     gdf=gdf,
     stats_functions=['PERC75'],
     intersections=intersections,
-    time_resolution=24
+    time_resolution=pd.Timedelta(hours=24)
 )
 print(dfs['PERC75'])
